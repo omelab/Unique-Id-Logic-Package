2f5cace2fdb6747d1c3b310734e1d1aff72903b9
Abu Bakar <a.bakar87@gmail.com>
2025-11-04 12:25:50 +0600

    module set global

--- src/id-logic.module.ts
+++ src/id-logic.module.ts
@@ -1,23 +1,16 @@
-import {
-  DynamicModule,
-  Module,
-} from '@nestjs/common';
+import { DynamicModule, Global, Module } from '@nestjs/common';
 import { SequelizeModule } from '@nestjs/sequelize';
 import { IdLogicService } from './id-logic.service';
 import { SysGeneratedId } from './models/generated-id.model';
 import { IdLogic } from './models/id-logic.model';
 
+@Global()
 @Module({})
 export class IdLogicModule {
   static register(): DynamicModule {
     return {
       module: IdLogicModule,
-      imports: [
-        SequelizeModule.forFeature([
-          IdLogic,
-          SysGeneratedId,
-        ]),
-      ],
+      imports: [SequelizeModule.forFeature([IdLogic, SysGeneratedId])],
       providers: [IdLogicService],
       exports: [IdLogicService],
     };
